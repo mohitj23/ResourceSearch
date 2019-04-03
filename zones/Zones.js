67ad13bd9d57af449ff21046660cd1e89f13aecc
Amrish Jhaveri <ajhave5@uic.edu>
2019-04-02 21:52:12 -0500

    k-ring(2 hop) valid neighbours.

--- zones/Zones.js
+++ zones/Zones.js
@@ -3,17 +3,33 @@ const manhattan_polygon = require("./manhattan_polygon_lat_lon.json");
 const fs = require("fs");
 const ZoneObj = require("./zone-model");
 
-const hexagons = h3.polyfill(manhattan_polygon, 9, true);
-
+const hexagonsMap = {};
 const zoneMap = {};
-const zones = JSON.stringify(getH3IndexFromH3Geo(hexagons));
 
-fs.writeFile("./manhattan_zones_lat_lon_3.json", JSON.stringify(zoneMap, undefined, 2), 'utf8', () => {
-    console.log(zoneMap);
-});
 
-function getH3IndexFromH3Geo(hexagons) {
-    return hexagons.map(hex => {
-        zoneMap[hex] = new ZoneObj(... h3.h3ToGeo(hex), hex, h3.kRingDistances(hex, 2));
+function writeToFile() {
+    fs.writeFile("./manhattan_zones_lat_lon_3.json", JSON.stringify(zoneMap, undefined, 2), 'utf8', () => {
+        console.log("Written to file");
     });
 }
+
+function getH3IndexFromH3Geo() {
+
+    const hexagons = h3.polyfill(manhattan_polygon, 9, true);
+
+    // put zones's h3Index to a Map
+    hexagons.map(hex => hexagonsMap[hex] = {});
+
+    // console.log(JSON.stringify(hexagonsMap));
+
+    hexagons.map(hex => {
+        // find the valid neighbours
+        const kRingNeighbours = h3.kRingDistances(hex, 2).map(zoneList => zoneList.filter(zone => hexagonsMap[zone]));
+        // console.log(kRingNeighbours);
+        zoneMap[hex] = new ZoneObj(... h3.h3ToGeo(hex), hex, kRingNeighbours);
+    });
+
+    writeToFile();
+}
+
+getH3IndexFromH3Geo();

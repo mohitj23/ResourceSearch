466ace9e49babf69551b6bc24927485212c99046
Amrish Jhaveri <ajhave5@uic.edu>
2019-04-06 20:25:51 -0500

    properties for Cab and Resources.

--- AllocationNavigation/src/main/java/com/uic/cs581/model/Cab.java
+++ AllocationNavigation/src/main/java/com/uic/cs581/model/Cab.java
@@ -2,7 +2,15 @@ package com.uic.cs581.model;
 
 import lombok.*;
 
+import java.util.List;
+
 @Builder
 public class Cab {
 
+    private int id;
+    private int resourceId;
+    private List<String> searchPaths;
+    private String futurePath;
+    private long nextAvailableTime;
+    private long totalSearchTime;
 }
--- AllocationNavigation/src/main/java/com/uic/cs581/model/Resource.java
+++ AllocationNavigation/src/main/java/com/uic/cs581/model/Resource.java
@@ -4,4 +4,12 @@ import lombok.Builder;
 
 @Builder
 public class Resource {
+    private int resourceId;
+    private String pickUpH3Index;
+    private String dropOffH3Index;
+    private String pickUpLat;
+    private String pickUpLong;
+    private String dropOffLat;
+    private String dropOffLong;
+    private int cabId;
 }

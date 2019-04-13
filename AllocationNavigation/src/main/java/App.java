a7aa1d31957a923fc8487d8e82b861c09053fcfe
Amrish Jhaveri <ajhave5@uic.edu>
2019-04-13 18:27:27 -0500

    Json zone reading data.

--- AllocationNavigation/src/main/java/App.java
+++ AllocationNavigation/src/main/java/App.java
@@ -1,8 +1,15 @@
+import com.uic.cs581.model.Zone;
 import com.uic.cs581.utils.BasicCSVReader;
 import lombok.extern.slf4j.Slf4j;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.type.TypeReference;
 
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.text.ParseException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.TimeZone;
 
 @Slf4j
@@ -16,6 +23,13 @@ public class App {
 
         // Read the test data csv
         BasicCSVReader.getResourcesFromTestData("test.csv");
+
+        // Read zone data from JSON file
+//        new FileReader("./src/main/resources/manhattan_zones_lat_lon_3.json");
+        HashMap<String,Zone> result =
+                new ObjectMapper().readValue(
+                        new File("./src/main/resources/manhattan_zones_lat_lon_3.json"),
+                        new TypeReference<Map<String, Zone>>() {});
 
         // append h3Index to the test data
 

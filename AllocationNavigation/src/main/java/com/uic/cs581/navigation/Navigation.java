208e3ba6b9d26a6895938d49e12e403d4c24bb6c
Chinmay Gangal <chinmay2312@gmail.com>
2019-04-17 10:33:20 -0500

    - Calling ZoneMap to get zones

--- AllocationNavigation/src/main/java/com/uic/cs581/navigation/Navigation.java
+++ AllocationNavigation/src/main/java/com/uic/cs581/navigation/Navigation.java
@@ -1,76 +1,50 @@
 package com.uic.cs581.navigation;
 
 import com.uic.cs581.model.Zone;
-import org.codehaus.jackson.map.ObjectMapper;
-import org.codehaus.jackson.type.TypeReference;
-
-import java.io.File;
-import java.io.FileReader;
+import com.uic.cs581.model.ZoneMap;
 import java.io.IOException;
 import lombok.extern.slf4j.Slf4j;
 import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.List;
 
 @Slf4j
 public class Navigation {
 
     public static void main(String[] args) throws IOException {
 
-        oneHop("89f05ab410bffff");
+        oneHop("892a10089dbffff");
+    }
+
+    static String kHops(String currZone)    {
+
+        return currZone;
     }
 
     static String oneHop(String currZone) throws IOException {
 
-        //TODO Get scores of neighbors from Uber-API: (API results stored in zones folder)
+        //Get scores of neighbors from Uber-API: (API results stored in zones folder)
+        Zone zone = ZoneMap.getZone(currZone);
+        List<List<String>> kRings = zone.kRingNeighbors;
+        List<String> kRing1 = kRings.get(1);
+        Double[] zoneScores = new Double[6];
+        for(int i=0;i<6;i++)    {
+            zoneScores[i]=ZoneMap.getZone(kRing1.get(i)).getScore();
 
-        /*
-        String absPath = new File("").getAbsolutePath();
-        //absPath = "\""+absPath+"\"";
-        //System.out.println("absPath: "+absPath);
-        String basePath = new File(absPath).getParent();
-        //System.out.println(basePath);
-
-
-        try(FileReader reader = new FileReader(basePath+"\\zones\\manhattan_zones_lat_lon_3.json"))  {
-
-            //System.out.println(basePath);
-            System.out.println("in try");
-
-
-        } catch (Exception e)   {
-            //System.out.println(basePath+" catch");
+            //TODO: remove below 2 lines.......placed because scores are null at the time of coding
+            //or replace a default value, current default value is the array-index
+            if(zoneScores[i]==null)
+                zoneScores[i]=(double)i;
         }
-        */
-
-        HashMap<String, Zone> result =
-                new ObjectMapper().readValue(
-                        new File("./src/main/resources/manhattan_zones_lat_lon_3.json"),
-                        new TypeReference<Map<String, Zone>>() {});
-
-        Double[] zoneScores;
-        //TODO
-        //Commented since scores are not available yet
-        /*
-            zoneScoreRanges= new Double[6];
-            int i=0;
-            zoneScoreRanges[i] = result.get(result.get(currZone).kRingNeighbors.get(1).get(0)).getScore();
-            for(String neighbor: result.get(currZone).kRingNeighbors.get(1).subList(1, 5))    {
-                zoneScoreRanges[++i] = zoneScoreRanges[i-1]+result.get(neighbor).getScore();
-            }
-         */
-
-        zoneScores = new Double[]{6.0, 5.0, 3.0, 1.0, 2.0, 4.0};
+        log.debug(Arrays.toString(zoneScores));
+        //zoneScores = new Double[]{6.0, 5.0, 3.0, 1.0, 2.0, 4.0};
 
         //Pick one neighbor non-deterministically
         int selectedIndex = nonDeterministic(zoneScores);
         log.debug("Selected index: "+selectedIndex);
 
-        //System.out.println(Arrays.toString(zoneScores));
-        log.debug(Arrays.toString(zoneScores));
 
-        //return h3-index of picked zone
-        return result.get(currZone).kRingNeighbors.get(1).get(selectedIndex);
+        //Return h3-index of picked zone
+        return kRing1.get(selectedIndex);
     }
 
     private static int nonDeterministic(Double[] scores)   {

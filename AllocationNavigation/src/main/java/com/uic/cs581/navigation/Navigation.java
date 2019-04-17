cfd03778b8c58c1890a20b9ad1b54e4cfeb18b33
Chinmay Gangal <chinmay2312@gmail.com>
2019-04-17 11:47:50 -0500

    - Overloaded oneHop() for previous-zone (near-future work: filter result based on incoming-direction)

--- AllocationNavigation/src/main/java/com/uic/cs581/navigation/Navigation.java
+++ AllocationNavigation/src/main/java/com/uic/cs581/navigation/Navigation.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import lombok.extern.slf4j.Slf4j;
 import java.util.Arrays;
 import java.util.List;
+import java.util.MissingResourceException;
 
 @Slf4j
 public class Navigation {
@@ -20,12 +21,18 @@ public class Navigation {
         return currZone;
     }
 
-    static String oneHop(String currZone) throws IOException {
+    static String oneHop(String currZone, String prevZone)   {
+
+        //TODO: [-90,+90] neighbor selection
+
+        Zone zone = ZoneMap.getZone(currZone);
+        List<String> kRing1 = zone.kRingNeighbors.get(1);
+
+        //TODO: replace with appropriate exception
+        if(!kRing1.contains(prevZone))
+            throw new MissingResourceException("prevZone not in currZone's neighbors", "String", "prevZone");
 
         //Get scores of neighbors from Uber-API: (API results stored in zones folder)
-        Zone zone = ZoneMap.getZone(currZone);
-        List<List<String>> kRings = zone.kRingNeighbors;
-        List<String> kRing1 = kRings.get(1);
         Double[] zoneScores = new Double[6];
         for(int i=0;i<6;i++)    {
             zoneScores[i]=ZoneMap.getZone(kRing1.get(i)).getScore();
@@ -47,9 +54,16 @@ public class Navigation {
         return kRing1.get(selectedIndex);
     }
 
+    static String oneHop(String currZone)  {
+
+        //random index
+        int rand = (int)(Math.random() * 6);
+        return oneHop(currZone, ZoneMap.getZone(currZone).kRingNeighbors.get(1).get(rand));
+    }
+
     private static int nonDeterministic(Double[] scores)   {
 
-        //cumulate scores to have ranges indirectly
+        //Cumulate scores to have ranges indirectly
         for(int i=1;i<scores.length;i++)
             scores[i] += scores[i-1];
 

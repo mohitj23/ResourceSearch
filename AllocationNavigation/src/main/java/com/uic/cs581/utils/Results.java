1ee056a66a36414b3b5358377fc6a4ac2e7c4d0d
Amrish Jhaveri <ajhave5@uic.edu>
2019-04-21 00:24:51 -0500

    parallel streams

--- AllocationNavigation/src/main/java/com/uic/cs581/utils/Results.java
+++ AllocationNavigation/src/main/java/com/uic/cs581/utils/Results.java
@@ -8,13 +8,13 @@ import lombok.extern.slf4j.Slf4j;
 public class Results {
 
     public static void avgSearchTimeOfAgents() {
-        double avgSearchTime = CabPool.getEntireCabPool().stream()
+        double avgSearchTime = CabPool.getEntireCabPool().parallelStream()
                 .reduce(0.0, (subtotal, element) -> subtotal + element.getTotalSearchTime(), Double::sum) / CabPool.getEntireCabPool().size();
         log.info("Average Search Time:\t " + avgSearchTime);
     }
 
     public static void avgIdleTimeOfAgents() {
-        double avgIdleTime = CabPool.getEntireCabPool().stream()
+        double avgIdleTime = CabPool.getEntireCabPool().parallelStream()
                 .reduce(0.0, (subtotal, element) -> subtotal + element.getTotalIdleTime(), Double::sum) / CabPool.getEntireCabPool().size();
         log.info("Average Idle Time:\t " + avgIdleTime);
     }
